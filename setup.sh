cd6685b4731702c86cbc7901ab96b11e85e9b105
Rhyuu <88861193+Rhyuu11@users.noreply.github.com>
2026-08-20 10:56:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -655,47 +655,47 @@ fun_bar() {
 
 
 res2() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/ssh-vpn.sh" /root/ssh-vpn.sh && chmod +x /root/ssh-vpn.sh && /root/ssh-vpn.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 clear
 } 
 
 res3() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/ins-xray.sh" /root/ins-xray.sh && chmod +x /root/ins-xray.sh && /root/ins-xray.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 clear
 }
 
 res4() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/sshws/insshws.sh" /root/insshws.sh && chmod +x /root/insshws.sh && /root/insshws.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
 }
 
 res5() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/set-br.sh" /root/set-br.sh && chmod +x /root/set-br.sh && /root/set-br.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 clear
 }
 
 res6() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/sshws/ohp.sh" /root/ohp.sh && chmod +x /root/ohp.sh && /root/ohp.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/sshws/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 clear
 }
 
 res7() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/menu/update.sh" /root/update.sh && chmod +x /root/update.sh && /root/update.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/menu/update.sh && chmod +x update.sh && ./update.sh
 clear
 }
 
 res8() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/slowdns/installsl.sh" /root/installsl.sh && chmod +x /root/installsl.sh && bash /root/installsl.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/slowdns/installsl.sh && chmod +x installsl.sh && bash installsl.sh
 clear
 }
 
 res9() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/udp-custom.sh" /root/udp-custom.sh && chmod +x /root/udp-custom.sh && bash /root/udp-custom.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/udp-custom.sh && chmod +x udp-custom.sh && bash udp-custom.sh
 clear
 }
 
 res10() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/noobz/noobzvpns.zip" /root/noobzvpns.zip
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/noobz/noobzvpns.zip
 unzip noobzvpns.zip
 chmod +x noobzvpns/*
 cd noobzvpns
@@ -705,12 +705,12 @@ clear
 }
 
 res11() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/bin/limit.sh" /root/limit.sh && chmod +x /root/limit.sh && /root/limit.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/bin/limit.sh && chmod +x limit.sh && ./limit.sh
 clear
 }
 
 res12() {
-wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/ins-trgo.sh" /root/ins-trgo.sh && chmod +x /root/ins-trgo.sh && /root/ins-trgo.sh
+wget https://raw.githubusercontent.com/Rhyuu11/sc2026/main/install/ins-trgo.sh && chmod +x ins-trgo.sh && ./ins-trgo.sh
 clear
 }
 
